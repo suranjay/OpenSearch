b082c718ffe3207ce4871512bb850b7645af5d8d
suranjay <surajkumar.tu@gmail.com>
2023-08-03 18:03:09 +0530

    add tmpfs

--- plugins/telemetry-otel/src/main/java/org/opensearch/telemetry/tracing/OTelResourceProvider.java
+++ plugins/telemetry-otel/src/main/java/org/opensearch/telemetry/tracing/OTelResourceProvider.java
@@ -47,7 +47,7 @@ public final class OTelResourceProvider {
         try {
             return get(
                 settings,
-                SpanDiskExporter.create(LoggingSpanExporter.create(), new File("/tmp/export"), StorageConfiguration.getDefault()),
+                SpanDiskExporter.create(LoggingSpanExporter.create(), new File("~/tmpfs"), StorageConfiguration.getDefault()),
                 ContextPropagators.create(W3CTraceContextPropagator.getInstance()),
                 Sampler.alwaysOn()
             );
